252feaf2ba98f0e1e6c607f6cbc1a65a61e9d45f
Deriam Suarez <deriam.suarez@mixart.do>
2023-12-06 14:40:41 -0400

    first commit

--- App.js
+++ App.js
@@ -1,10 +1,12 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, StatusBar } from "react-native";
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <View style={{ backgroundColor: "green", padding: 16, borderRadius: 12 }}>
+        <Text>Gg</Text>
+      </View>
+      <Text>klk Dariel el mejor</Text>
       <StatusBar style="auto" />
     </View>
   );
@@ -13,8 +15,6 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
   },
 });
